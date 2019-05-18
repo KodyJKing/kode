f17536c3a4217815419bfd30e779a7f9fdc2df94
KodyJKing <kody.j.king@gmail.com>
2019-05-17 19:28:51 -0700

    Removed explicit triple char arrays, halving runtime.

--- src/data/strings/suffixArray.ts
+++ src/data/strings/suffixArray.ts
@@ -1,108 +1,84 @@
 import { merge } from "../sorting/merge";
 
-type triple = [ number, number, number ]
-type char = number | triple
-type chars = char[]
-
 const NULL_CHAR = -1
-
-function compareChar( a: char, b: char ) {
-    if ( typeof a == "number" && typeof b == "number" )
-        return a - b
-    if ( Array.isArray( a ) && Array.isArray( b ) )
-        return lexCompare( a, b )
-    return 0
-}
 
 function defaultToNull( char: number ) {
     return char == undefined ? NULL_CHAR : char
 }
 
-function lexCompare( a: number[], b: number[] ) {
-    for ( let i = 0; i < a.length; i++ ) {
-        let d = defaultToNull( a[ i ] ) - defaultToNull( b[ i ] )
-        if ( d != 0 ) return d
-    }
+function lexCompare3( a: number, b: number, c: number, x: number, y: number, z: number ) {
+    let d = defaultToNull( a ) - defaultToNull( x )
+    if ( d != 0 ) return d
+    d = defaultToNull( b ) - defaultToNull( y )
+    if ( d != 0 ) return d
+    d = defaultToNull( x ) - defaultToNull( z )
+    if ( d != 0 ) return d
     return 0
 }
 
-function removeDuplicates( arr: any[], key = ( e ) => e.toString() ) {
-    let result: any[] = []
-    let visited = {}
-    for ( let elem of arr ) {
-        let k = key( elem )
-        if ( visited[ k ] ) continue
-        visited[ k ] = true
-        result.push( elem )
-    }
-    return result
+function lexCompare2( a: number, b: number, x: number, y: number ) {
+    let d = defaultToNull( a ) - defaultToNull( x )
+    if ( d != 0 ) return d
+    d = defaultToNull( b ) - defaultToNull( y )
+    if ( d != 0 ) return d
+    return 0
 }
 
-// TODO: Do this implicitly. Don't actually create the triples.
-// Just take the indices and base string.
-// Also, replace all sorts with radix sort to achieve linear time.
-function replaceCharactersWithRank( str: chars ) {
-    let alphabet = removeDuplicates( str ) as chars
-    alphabet.sort( compareChar )
+function contractAndRank( str: number[], verbose: boolean ) {
+    let indices = range( str.length )
+    // TODO: Make this radix sort for linear time.
+    indices.sort(
+        ( i, j ) => {
+            for ( let k = 0; k < 3; k++ ) {
+                let d = defaultToNull( str[ i + k ] ) - defaultToNull( str[ j + k ] )
+                if ( d != 0 ) return d
+            }
+            return 0
+        }
+    )
 
-    let ranks: { [ name: string ]: number } = {}
+    let ranks = new Array( str.length )
+    let c0 = -1
+    let c1 = -1
+    let c2 = -1
     let rank = 0
-    for ( let char of alphabet )
-        ranks[ char.toString() ] = rank++
+    for ( let index of indices ) {
+        if ( str[ index ] != c0 || str[ index + 1 ] != c1 || str[ index + 2 ] != c2 ) {
+            rank++
+            c0 = str[ index ]
+            c1 = str[ index + 1 ]
+            c2 = str[ index + 2 ]
+        }
+        ranks[ index ] = rank
+    }
 
-    let result: number[] = []
-    for ( let char of str )
-        result.push( ranks[ char.toString() ] )
-
-    return result
+    return str.map( ( v, i ) => ranks[ i ] )
 }
 
-// function contractAndRank( str: number[], verbose: boolean ) {
-//     let indices = str.map( ( v, i ) => i )
-//     indices = removeDuplicates( indices, ( i ) => str.slice( i, i + 3 ) )
-//     indices.sort( ( i, j ) => lexCompare( str.slice( i, i + 3 ), str.slice( j, j + 3 ) ) )
-
-//     let ranks: { [ name: string ]: number } = {}
-//     let rank = 0
-//     for ( let i of indices )
-//         ranks[ str.slice( i, i + 3 ).toString() ] = rank++
-
-//     if ( verbose ) {
-//         console.log( str.map( n => String.fromCharCode( n ) ).join( "" ) )
-//         console.log( indices )
-//         console.log( ranks )
-//         console.log()
-//     }
-
-//     return str.map( ( v, i ) => ranks[ str.slice( i, i + 3 ).toString() ] )
-// }
+function range( n: number ) {
+    let result = new Array( n )
+    for ( let i = 0; i < n; i++ ) result[ i ] = i
+    return result
+}
 
 export default function suffixArray( content: string ) {
     function suffixArray( str: number[], verbose = false ): number[] {
         if ( str.length == 1 )
             return [ 0 ]
         if ( str.length == 2 ) {
-            if ( compareChar( str[ 0 ], str[ 1 ] ) < 0 )
+            if ( str[ 0 ] - str[ 1 ] < 0 )
                 return [ 0, 1 ]
             return [ 1, 0 ]
         }
 
-        let rankedStr = replaceCharactersWithRank( str )
-        // let rankedStr2 = contractAndRank( str, verbose )
+        let rankedStr = contractAndRank( str, verbose )
 
-        // if ( verbose ) {
-        //     console.log( rankedStr )
-        //     console.log( rankedStr2 )
-        // }
-
-        let R0: chars = []
-        let R1: chars = []
-        let R2: chars = []
-        let RN: chars[] = [ R0, R1, R2 ]
-        for ( let i = 0; i < rankedStr.length; i++ ) {
-            let char: triple = [ rankedStr[ i ], rankedStr[ i + 1 ], rankedStr[ i + 2 ] ]
-            RN[ i % 3 ].push( char )
-        }
+        let R0: number[] = []
+        let R1: number[] = []
+        let R2: number[] = []
+        let RN: number[][] = [ R0, R1, R2 ]
+        for ( let i = 0; i < rankedStr.length; i++ )
+            RN[ i % 3 ].push( rankedStr[ i ] )
 
         let R01 = R0.concat( R1 )
 
@@ -114,40 +90,43 @@ export default function suffixArray( content: string ) {
         for ( let i = 0; i < suffixArray01.length; i++ )
             ranks[ suffixArray01[ i ] ] = i
 
-        let suffixArray2: number[] = R2
-            .map( ( v, i ) => i )
-            .sort( ( i, j ) => lexCompare(
-                [ rankedStr[ 3 * i + 2 ], ranks[ 3 * i + 3 ] ],
-                [ rankedStr[ 3 * j + 2 ], ranks[ 3 * j + 3 ] ]
-            )
-            )
-            .map( nonSampleIndexToIndex )
+        let compare02 = ( i, j ) => lexCompare2(
+            rankedStr[ i ], ranks[ i + 1 ],
+            rankedStr[ j ], ranks[ j + 1 ]
+        )
+        let compare12 = ( i, j ) => lexCompare3(
+            rankedStr[ i ], rankedStr[ i + 1 ], ranks[ i + 2 ],
+            rankedStr[ j ], rankedStr[ j + 1 ], ranks[ j + 2 ]
+        )
+        let compare22 = ( i, j ) => lexCompare2(
+            rankedStr[ i ], ranks[ i + 1 ],
+            rankedStr[ j ], ranks[ j + 1 ]
+        )
 
-        // if (verbose) {
-        //     console.log(rankedStr)
-        //     console.log()
-        //     console.log(R01)
-        //     console.log(R2)
-        //     console.log()
-        //     console.log(SA01)
-        //     console.log(SA2)
-        //     console.log()
-        // }
+        let suffixArray2: number[] = new Array( R2.length )
+        for ( let i = 0; i < R2.length; i++ )
+            suffixArray2[ i ] = nonSampleIndexToIndex( i )
+        suffixArray2.sort( compare22 )
+
+        if ( verbose ) {
+            // console.log(rankedStr)
+            // console.log()
+            // console.log(R01)
+            // console.log(R2)
+            // console.log()
+            // console.log(SA01)
+            // console.log(SA2)
+            // console.log()
+        }
 
         return merge( suffixArray01, suffixArray2, ( i, j ) => {
-            if ( i % 3 == 0 ) {
-                return lexCompare(
-                    [ rankedStr[ i ], ranks[ i + 1 ] ],
-                    [ rankedStr[ j ], ranks[ j + 1 ] ]
-                )
-            } else {
-                return lexCompare(
-                    [ rankedStr[ i ], rankedStr[ i + 1 ], ranks[ i + 2 ] ],
-                    [ rankedStr[ j ], rankedStr[ j + 1 ], ranks[ j + 2 ] ]
-                )
-            }
+            if ( i % 3 == 0 )
+                return compare02( i, j )
+            else
+                return compare12( i, j )
         } )
     }
+
     let str = Array.from( content ).map( ( char ) => char.charCodeAt( 0 ) )
     return suffixArray( str, true )
 }
